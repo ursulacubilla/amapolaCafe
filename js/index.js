74f203186fe652e3340ea0e5ed34b31d02aae712
Ursula Cubilla <ursulacubilla@gmail.com>
2023-10-30 16:20:15 +0100

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// ormulario de reserva de mesa.
+document.getElementById('form-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var name = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var date = document.getElementById('fecha').value;
+    var time = document.getElementById('hora').value;
+    
+    alert('¡Gracias por reservar, ' + name + '! Tu mesa está reservada para el ' + date + ' a las ' + time + '. Te esperamos.');
+
+    document.getElementById('form-contacto').reset();
+});
+
+// Formulario de reserva de pasteles.
+    document.getElementById('contacto-reservas').addEventListener('click', function(e){
+        let seccionReserva = e.target.parentNode.parentNode;
+        seccionReserva.style.display = "none";
+        let formReservaPastel = document.querySelector('#reserva-pastel');
+        formReservaPastel.innerHTML = `
+            <form id="contactForm" class="form-pedido-pastel">
+                    <label for="name">Nombre:</label>
+                    <input type="text" id="name" required class="input-pastel">
+              
+                    <label for="email">Correo Electrónico:</label>
+                    <input type="email" id="email" required class="input-pastel">
+               
+                    <label for="message">Mensaje:</label>
+                    <textarea id="message" required placeholder="Tu pedido: describe aqui como quieres que sea tu pastel, peso, color, sabores, elergias, etc."></textarea>
+                
+                <button type="submit" class="enviar-pedido">Enviar Pedido</button>
+            </form>
+        `;
+
+        const contactForm = document.getElementById('contactForm');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        
+        alert('¡Pedido realizado con éxito!');
+
+        formReservaPastel.innerHTML = ''; 
+        seccionReserva.style.display = "block";
+    });
+    });
